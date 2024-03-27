4351c4909ec18997b0e45b92f57fdf23206f3cb3
David Whitney <david@davidwhitney.co.uk>
2024-03-27 11:22:55 +0000

    formatting

--- app/src/components/Navigation.tsx
+++ app/src/components/Navigation.tsx
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 export default function Navigation() {
     return (
         <nav>            
-        <Link to="/">
-            <img src="/images/white-logo.png" alt="Dovetail" width={100} height={80} />
-        </Link>
-        <ul>
-            <li><Link to="/cocktail-menu">Cocktail Menu</Link></li>
-            <li><Link to="/whats-on">What's On</Link></li>
-            <li><Link to="/private-hire">Private Hire</Link></li>
-            <li><Link to="/cocktail-making-class">Masterclasses</Link></li>
-        </ul>
-        <Link to="/bookings" className="buttonLink">Book Now</Link>
+            <Link to="/">
+                <img src="/images/white-logo.png" alt="Dovetail" width={100} height={80} />
+            </Link>
+            <ul>
+                <li><Link to="/cocktail-menu">Cocktail Menu</Link></li>
+                <li><Link to="/whats-on">What's On</Link></li>
+                <li><Link to="/private-hire">Private Hire</Link></li>
+                <li><Link to="/cocktail-making-class">Masterclasses</Link></li>
+            </ul>
+            <Link to="/bookings" className="buttonLink">Book Now</Link>
     </nav>
     );
 }
