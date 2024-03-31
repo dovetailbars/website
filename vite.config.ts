1a32c052ac915a6b42fab51813705176bb3b3936
David Whitney <david@davidwhitney.co.uk>
2024-03-31 07:53:17 +0100

    Moved everything up a level.

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    root: "app",
+    root: "./",
     server: {
         port: 8080,
         strictPort: true,
-        host: true,
-        proxy: {
-            '/api': 'http://localhost:7071'
-        }
+        host: true
     },
     build: {
-        outDir: "../dist",
+        outDir: "./dist",
         sourcemap: true,
         emptyOutDir: true
     },
