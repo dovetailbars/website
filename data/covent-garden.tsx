636bbc9b286d0aa224ec141efd29896bb2936edf
David Whitney <david@davidwhitney.co.uk>
2024-04-21 16:55:30 +0100

    Use split content component in private hire

--- data/covent-garden.tsx
+++ data/covent-garden.tsx
@@ -68,34 +68,32 @@ export default {
     </>),
 
     privateHireContent: (<>
-        <section className="secondary">                
-            <div className="centeredContent split">
-                <div>
-                    <h2>The Venue</h2>
-                    <p>
-                        Nestled comfortably above the bustling Covent Garden Market, by the West End, The Dovetail Bar is your hideaway for all private affairs. In a hidden room, we serve creative yet unpretentious cocktails in a refined intimate environment. The ultimate hideaway from London’s busy streets, the West End’s best kept secret. 
-                    </p>
-                    <p>
-                        Perfect for:
-                    </p>
-                    <ul>
-                        <li>Birthdays</li>
-                        <li>Private Events</li>
-                        <li>Cocktail Masterclasses</li>
-                    </ul>
-                    <p className="paddedTop">
-                        <Link to="/private-hire#contact" className="buttonLink alternate">Hire the space</Link>
-                    </p>
-                </div>
-                <div>
-                    <h2>Walkthrough</h2>
-                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/jjHQq6TpoUo?si=TLL7W-orQ4OxbyHx" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
-                    <p>
-                        Watch the short video above that gives you an idea of the unique intimate space.
-                    </p>
-                </div>
+        <SplitContent>
+            <div>
+                <h2>The Venue</h2>
+                <p>
+                    Nestled comfortably above the bustling Covent Garden Market, by the West End, The Dovetail Bar is your hideaway for all private affairs. In a hidden room, we serve creative yet unpretentious cocktails in a refined intimate environment. The ultimate hideaway from London’s busy streets, the West End’s best kept secret. 
+                </p>
+                <p>
+                    Perfect for:
+                </p>
+                <ul>
+                    <li>Birthdays</li>
+                    <li>Private Events</li>
+                    <li>Cocktail Masterclasses</li>
+                </ul>
+                <p className="paddedTop">
+                    <Link to="/private-hire#contact" className="buttonLink alternate">Hire the space</Link>
+                </p>
             </div>
-        </section>
+            <div>
+                <h2>Walkthrough</h2>
+                <iframe width="100%" height="315" src="https://www.youtube.com/embed/jjHQq6TpoUo?si=TLL7W-orQ4OxbyHx" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
+                <p>
+                    Watch the short video above that gives you an idea of the unique intimate space.
+                </p>
+            </div>
+        </SplitContent>
 
         <section className="primary">                
             <div id="contact" className="centeredContent centerAligned">
