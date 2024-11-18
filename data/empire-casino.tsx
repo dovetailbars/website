ce4a844f61ff3be4de8ced61138935dd3fb4ed59
David Whitney <david@davidwhitney.co.uk>
2024-11-18 18:02:41 +0000

    better responsive logo, better maps point for empire.

--- data/empire-casino.tsx
+++ data/empire-casino.tsx
@@ -9,7 +9,7 @@ const name = "Dovetail " + venueName;
 export default {
     id, name, venueName,
     
-    googleMapsLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.1279768651034!2d-0.1329298219507423!3d51.51086807181415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487604d208c64f53%3A0x3be1912a868e5914!2sEmpire%20Casino!5e0!3m2!1sen!2suk!4v1730112526504!5m2!1sen!2suk",
+    googleMapsLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.1239543567253!2d-0.13305262195072406!3d51.510941871814346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487605ad4d2c62ef%3A0xfad514e573027f97!2sDovetail%20Bar%20at%20Empire%20Casino!5e0!3m2!1sen!2suk!4v1731952901311!5m2!1sen!2suk",
     
     designMyNightConfig: {
         venueId: "671909df81884a4d4032ffca",
