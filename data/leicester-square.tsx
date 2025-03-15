692dd6a4b19ec957342b0a060420d31a53ed3b87
David Whitney <david@davidwhitney.co.uk>
2025-03-15 18:29:35 +0000

    Copy updates

--- data/leicester-square.tsx
+++ data/leicester-square.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import IntroSection from "../src/components/IntroSection";
 import SplitContent from "../src/components/SplitContent";
+import { ContactForm } from "../src/components/ContactForm";
 
 const id = "leicester-square";
 const venueName = "Leicester Square";
@@ -23,8 +24,8 @@ export default {
     },
 
     location: "Balcony Bar, 5-6 Leicester Square, London WC2H 7NA",
-    heroImage: "/images/empire-hero2-sm.jpg",
-    spotlightImage: "/images/empire-casino.png",
+    heroImage: "/images/leicester-hero2-sm.webp",
+    spotlightImage: "/images/DSC05840.webp",
     
     spotlightText: (
     <>
@@ -32,7 +33,7 @@ export default {
             Dovetail's newest offering is a joint venture with Empire Casino, that sees us rebranding their balcony bar in time for Summer.
         </p>
         <p>
-            With views of the bustling crowds in Leicester Square, it's the perfect place to enjoy a spritz before coming inside for an evening of live DJs and perfectly crafted cocktails.
+            With views of the bustling crowds in Leicester Square, it's the perfect place to enjoy a spritz before coming inside for an evening of perfectly crafted cocktails.
         </p>
     </>),
 
@@ -49,14 +50,58 @@ export default {
         <section className="primary">
             <Link to="bookings" className="buttonLink">Book Now</Link>
         </section>
-
         
         <SplitContent>
             <div>
-                <img src="/images/empire1.jpg" alt="Private Hire" />
+                <img src="/images/DSC05743.webp" alt="Private Hire" />
             </div>
             <div>
-                <img src="/images/empire2.jpg" alt="Private Hire" />
+                <h2>The Balcony</h2>
+                <p>
+                   The balcony overlooks the heart of Leicester Square, in what was once the The Empire Theatre. Reminiscent of a Parisian boulevard, this architecturally stunning space will elevate any event.
+                </p>
+            </div>
+        </SplitContent>
+
+        <SplitContent>
+            <div>
+                <img src="/images/DSC05717.webp" alt="Private Hire" />
+            </div>
+            <div>
+                <h2>Perfect for private hire</h2>
+                <p>
+                    Whether you're planning an intimate gathering, a celebration to remember, or a corporate meeting, Dovetail offers a unique setting that combines the charm of a secret speakeasy with the warmth of hospitality.
+                </p>
+                <p>
+                    Dovetail is more than just a venue; it's an experience waiting to be tailored to your every need. Our creative cocktails, served with a flair for the unpretentious, promise to enchant your guests, while our refined yet cozy environment ensures a memorable occasion for all.
+                </p>
+                <Link to="/leicester-square/private-hire" className="buttonLink">See the space</Link>
+            </div>
+        </SplitContent>
+    </>),
+
+
+    privateHireContent: (<>
+        <SplitContent>
+            <div>
+                <h2>The Venue</h2>
+                <p>
+                    Dovetail Bar - Leicester Square is the perfect venue for your West End visit, a Soho gem in the making. Enjoy a perfectly crafted cocktail in the sun on our balcony overlooking the bustling crowds below, then head inside for an evening of DJs, good drinks and great company.
+                </p>
+                <p>
+                    Perfect for:
+                </p>
+                <ul>
+                    <li>Birthdays</li>
+                    <li>Private Events</li>
+                    <li>Cocktail Masterclasses</li>
+                </ul>
+                <p className="paddedTop">
+                    <Link to="/private-hire#contact" className="buttonLink">Hire the space</Link>
+                </p>
+            </div>
+            <div>
+                <img src="/images/gallery/venues/leicestersquare/leicester2.webp" alt="Private Hire" />
             </div>
         </SplitContent>
     </>)
