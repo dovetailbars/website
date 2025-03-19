a0d1f1f2f0b792bd26c3517adff9e4a00dde76c8
David Whitney <david@davidwhitney.co.uk>
2025-03-19 00:10:21 +0000

    New Gallery.

--- src/components/StandardHero.tsx
+++ src/components/StandardHero.tsx
@@ -1,16 +1,24 @@
 export default function StandardHero({ heading = "", subheading = "", children = null }) {
 
-    const subheadMarkup = subheading != "" ? (<h3>{subheading}</h3>) : (<></>);
-    const childrenMarkup = children ? (<p>{children}</p>) : (<></>);
+    if (heading === "" && subheading === "" && children === null) {
+        return (<></>);
+    }
+
+    if (heading === "" && subheading === "") {
+        return (<>{children}</>);
+    }
 
     if (subheading === "" && children === null) {
         return (<h2 className="headerTitle">{ heading }</h2>);
     }
+    
+    const headingMarkup = heading != "" ? (<h2>{heading}</h2>) : (<></>);
+    const subheadMarkup = subheading != "" ? (<h3>{subheading}</h3>) : (<></>);
+    const childrenMarkup = children ? (<p>{children}</p>) : (<></>);
 
     return (<>
-        <h2>{heading}</h2>
-            { subheadMarkup }
-  
+        { headingMarkup }
+        { subheadMarkup }  
         { childrenMarkup }
     </>)
 }
--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,29 +1,41 @@
+import ImageCarousel from "../components/ImageCarousel";
 import { Layout } from "../components/Layout";
 import StandardHero from "../components/StandardHero";
 
 export default function() {
     const hero = {
-        image: "/images/DTB-Initial-Cocktail-Shoot-LR-020.jpg",
+        image: "",
         content: <StandardHero heading="Gallery" />
     };
 
-    const numberRange = (start: number, end: number) => Array.from({length: (end - start)}, (v, k) => k + start);
-    const rangeArray = numberRange(1, 16);
-    const images = rangeArray.map(i => `/images/gallery/image_${i}-min.jpg`);
-
-    const ordered = images.sort(() => Math.random() - 0.5);
+    const galleryImages = [
+        "/images/gallery/general/image_4-min.webp",
+        "/images/gallery/general/iceblock.webp",
+        "/images/gallery/general/image_12-min.webp",
+        "/images/gallery/drinks/armyandnavy.webp",
+        "/images/gallery/drinks/bloodymary.webp",
+        "/images/gallery/drinks/bramble.webp",
+        "/images/gallery/drinks/bramble2.webp",
+        "/images/gallery/drinks/cubalibre.webp",
+        "/images/gallery/drinks/cubalibre2.webp",
+        "/images/gallery/drinks/DSC04255.webp",
+        "/images/gallery/drinks/DSC06366.webp",
+        "/images/gallery/drinks/image_8-min.webp",
+        "/images/gallery/drinks/image_11-min.webp",
+        "/images/gallery/drinks/margarita.webp",
+        "/images/gallery/drinks/margarita2.webp",
+        "/images/gallery/drinks/oldfashioned.webp",
+        "/images/gallery/drinks/oldfashioned2.webp",
+        "/images/gallery/drinks/pinacolada.webp",
+        "/images/gallery/drinks/pinklady.webp",
+        "/images/gallery/drinks/pornstar.webp",
+        "/images/gallery/drinks/redhook.webp"
+    ];
 
     return (
         <Layout hero={hero}>
-            <section className="secondary">
-                <div className="centeredContent centerAligned">
-                            
-                    <div className="gallery">
-                        {ordered.map((image, index) => (
-                            <img key={index} src={image} alt={`Gallery image ${index}`} />
-                        ))}
-                    </div>
-                </div>
+            <section id="galleryItems">
+                <ImageCarousel images={galleryImages} />
             </section>
         </Layout>
     );
