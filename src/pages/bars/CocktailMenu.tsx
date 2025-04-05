a4007787afb302bf10685d6b2e446884cd6cfa01
Jo Franchetti <jofranchetti@gmail.com>
2025-04-05 17:01:01 +0100

    update graphics and style menu

--- src/pages/bars/CocktailMenu.tsx
+++ src/pages/bars/CocktailMenu.tsx
@@ -2,29 +2,31 @@ import { Navigate, useParams } from "react-router-dom";
 import { Layout } from "../../components/Layout";
 import StandardHero from "../../components/StandardHero";
 import { getBarData } from "../../../data";
+import CocktailItem from "../../components/CocktailItem";
 
 export default function BarCocktailMenu() {
     const { barId } = useParams();
     const bar = getBarData(barId);
-    
+
     if (!bar) {
         return <Navigate replace to="/" />
     }
 
     const hero = {
         image: bar.heroImage || "/images/DTB-Initial-Cocktail-Shoot-LR-020.jpg",
-        content: <StandardHero heading={`${bar.name} Cocktail Menu`} />
+        content: <StandardHero heading={bar.name} subheading="Cocktail Menu" />
     };
 
     return (
         <Layout hero={hero}>
-            <section className="secondary">
-                <div className="centeredContent centerAligned">
-                    {bar.menuImage ? (
-                        <img src={bar.menuImage} alt={`${bar.name} Cocktail Menu`} className="menufullWidth" />
-                    ) : (
-                        <img src="/images/Dovetail-Menu.jpg" alt="Cocktail Menu" className="menufullWidth" />
-                    )}
+            <section className="cocktailmenu">
+                <h1>House Cocktails</h1>
+                <p>All house cocktails £{bar.cocktailPrice}, classics available on request</p>
+                <p></p>
+                <div className="cocktailmenu_container">
+                    {bar.menu.map((cocktail) => (
+                        <CocktailItem key={cocktail.name} cocktail={cocktail} />
+                    ))}
                 </div>
             </section>
         </Layout>
