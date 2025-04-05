6c2d203289dbadd4439a56b962c641596464d682
David Whitney <david@davidwhitney.co.uk>
2025-04-05 17:06:48 +0100

    Added main cocktail menu

--- src/pages/CocktailMenu.tsx
+++ src/pages/CocktailMenu.tsx
@@ -1,3 +1,5 @@
+import drinks from "../../data/drinks";
+import CocktailItem from "../components/CocktailItem";
 import { Layout } from "../components/Layout";
 import StandardHero from "../components/StandardHero";
 
@@ -7,11 +9,17 @@ export default function() {
         content: <StandardHero heading="Cocktail Menu" />
     };
 
+    const menu = drinks.values();
+
     return (
         <Layout hero={hero}>
-            <section className="secondary">
-                <div className="centeredContent centerAligned">
-                    <img src="/images/Dovetail-Menu.jpg" alt="Cocktail Menu" className="menufullWidth" />
+            <section className="cocktailmenu">
+                <h1>House Cocktails</h1>
+                <p>Our drinks are playful reimaginings of London’s most popular drinks. We have taken the best of the familiar, reworking them into new and exciting concoctions, whilst respecting the flavours that made them beloved.</p>
+                <div className="cocktailmenu_container">
+                    {menu.map((cocktail) => (
+                        <CocktailItem key={cocktail.name} cocktail={cocktail} />
+                    ))}
                 </div>
             </section>
         </Layout>
