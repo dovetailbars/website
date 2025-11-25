bc84fb9fb044a8c4f926ed5914f673ae03f9b8e9
David Whitney <david@davidwhitney.co.uk>
2025-11-25 20:34:06 +0000

    prep intro for having an optional background.

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -2,13 +2,14 @@ type Props = {
     title?: string
     children: React.ReactNode;
     className?: string;
+    spotlightImage?: string;
 };
 
-export default function IntroSection({ title, children, className }: Props) {
+export default function IntroSection({ title, children, className, spotlightImage }: Props) {
     className = className || "";
     
     return (<>
-        <section className={`secondary ${className}`}>
+        <section className={`secondary ${className}`} >
             <div className="intro">
                 { title && <h2>{title}</h2> }
                 { children }                  
